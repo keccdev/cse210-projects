b3942ce44892fc206d24de57348fd48ea9225333
keccdev <kecc.contact@gmail.com>
2024-04-25 17:31:29 -0600

    Subiendo cambios de 3ra tarea.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playAgain = true;
+
+            while (playAgain)
+            {
+                // Generate a random magic number between 1 and 100
+                Random random = new Random();
+                int magicNumber = random.Next(1, 101);
+
+                // User Interaction and Riddles
+                Console.WriteLine("Welcome to the Guess My Number game!");
+                Console.WriteLine("I'm thinking of a number between 1 and 100. Can you guess what it is?");
+
+                int userGuess;
+                int guessCount = 0;
+                bool guessedCorrectly = false;
+
+                // Loop until the user guesses correctly
+                while (!guessedCorrectly)
+                {
+                    Console.Write("What's your guess? ");
+                    userGuess = Convert.ToInt32(Console.ReadLine());
+                    guessCount++;
+
+                    if (userGuess < magicNumber)
+                    {
+                        Console.WriteLine("No, the number I'm thinking of is higher than " + userGuess + ". Try again!");
+                    }
+                    else if (userGuess > magicNumber)
+                    {
+                        Console.WriteLine("No, the number I'm thinking of is lower than " + userGuess + ". Try again!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Correct! The magic number was " + magicNumber + ". Great job!");
+                        guessedCorrectly = true;
+                    }
+                }
+
+                Console.WriteLine("Number of attempts: " + guessCount);
+
+                Console.Write("Do you want to play again? (yes/no): ");
+                string playAgainInput = Console.ReadLine().ToLower();
+                playAgain = (playAgainInput == "yes");
+            }
+
+            Console.WriteLine("Thanks for playing!");
     }
 }
+
