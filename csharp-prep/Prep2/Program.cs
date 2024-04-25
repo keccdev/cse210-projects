c9dbd2c791351784567d63b1d9331a8b2626df7d
keccdev <kecc.contact@gmail.com>
2024-04-25 12:42:12 -0600

    subiendo cambios

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your percentage grade: ");
+            double percentage = double.Parse(Console.ReadLine());
+
+            // Qualification statements
+            char letterGrade;
+            if (percentage >= 90)
+                letterGrade = 'A';
+            else if (percentage >= 80)
+                letterGrade = 'B';
+            else if (percentage >= 70)
+                letterGrade = 'C';
+            else if (percentage >= 60)
+                letterGrade = 'D';
+            else
+                letterGrade = 'F';
+
+             // Determine the sign (+ or -)
+        char sign = ' ';
+        int lastDigit = (int)(percentage % 10);
+        if (lastDigit >= 7)
+            sign = '+';
+        else if (lastDigit < 3)
+            sign = '-';
+
+            // Print the letter grade
+            Console.WriteLine($"Your letter grade is: {letterGrade}{sign}");
+
+            // Check if the student passed the course
+            if (percentage >= 70)
+                Console.WriteLine("Congratulations! You passed the course.");
+            else
+                Console.WriteLine("Keep working hard for next time.");
     }
 }
