2c6f7d49c31d183879462139803a2b09b4fa5ee3
Kevin Cruz <kecc.contact@gmail.com>
2024-04-26 21:19:11 -0600

    Subiendo cambios de 5ta tarea

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,49 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+     static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            return Console.ReadLine();
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string input = Console.ReadLine();
+            int number;
+            if (int.TryParse(input, out number))
+            {
+                return number;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+                return PromptUserNumber();
+            }
+        }
+
+        static int SquareNumber(int number)
+        {
+            return number * number;
+        }
+
+        static void DisplayResult(string name, int squaredNumber)
+        {
+            Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
+        }
+
+        static void Main(string[] args)
+        {
+            DisplayWelcome();
+            string userName = PromptUserName();
+            int userNumber = PromptUserNumber();
+            int squaredNumber = SquareNumber(userNumber);
+            DisplayResult(userName, squaredNumber);
     }
 }
